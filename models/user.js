5962e1a979a55ccd9fc095445c066b8100f69d08
Akhil <akhilpatyal49@gmail.com>
2024-03-05 20:52:23 +0530

    first commit

---
+++ models/user.js
@@ -0,0 +1,22 @@
+// database
+const mongoose = require("mongoose")
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: true,
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        require: true,
+    },
+
+}, {
+    timestamps: true
+})
+
+const User=mongoose.model(user,UserSchema);
